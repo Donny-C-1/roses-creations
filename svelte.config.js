1848becaf5f1047b232bb50a9589c8e780144fb7
Donny C <chikwemdonald@gmail.com>
2026-02-23 04:19:24 +0100

    fix: remove base path configuration from Svelte adapter setup

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		paths: { base: process.env.NODE_ENV === 'production' ? '/roses-creations' : '' }
+		adapter: adapter()
 	}
 };
 
